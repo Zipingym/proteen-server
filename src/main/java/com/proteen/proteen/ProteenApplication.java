5243d4048f5f16e924f13196123264667f50289a
jiseok419 <pyosik0419@gmail.com>
2023-04-03 14:20:49 +0900

    :tada: :: 프로젝트 시작

---
+++ src/main/java/com/proteen/proteen/ProteenApplication.java
@@ -0,0 +1,13 @@
+package com.proteen.proteen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProteenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProteenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/proteen/proteen/ProteenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proteen.proteen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProteenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
