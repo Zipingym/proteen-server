50db51b2906256ae41ef26ad03cd52a9ba80f27c
jiseok419 <pyosik0419@gmail.com>
2023-04-12 14:32:12 +0900

    :sparkles: :: User Service 클래스

---
+++ src/main/java/com/proteen/proteen/domain/user/service/UserService.java
@@ -0,0 +1,9 @@
+package com.proteen.proteen.domain.user.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+}
