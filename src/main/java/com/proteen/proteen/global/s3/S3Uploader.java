01425e2a2df03ff46a4f689a75df0ef3c7c03c8a
jiseok419 <pyosik0419@gmail.com>
2023-04-18 15:44:17 +0900

    :zap: :: S3 관련 서비스 구현

---
+++ src/main/java/com/proteen/proteen/global/s3/S3Uploader.java
@@ -0,0 +1,56 @@
+package com.proteen.proteen.global.s3;
+
+import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.model.AmazonS3Exception;
+import com.amazonaws.services.s3.model.CannedAccessControlList;
+import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.amazonaws.services.s3.model.PutObjectRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.UUID;
+
+@Component
+@RequiredArgsConstructor
+public class S3Uploader {
+
+    private final AmazonS3Client amazonS3Client;
+
+    @Value("${cloud.aws.s3.bucket}")
+    private String bucket;
+
+    public String uploadImage(MultipartFile file) {
+        try {
+            String originName = UUID.randomUUID().toString();
+            long size = file.getSize();
+
+            ObjectMetadata objectMetadata = new ObjectMetadata();
+            objectMetadata.setContentType(file.getContentType());
+            objectMetadata.setContentLength(size);
+
+            amazonS3Client.putObject(
+                    new PutObjectRequest(bucket, originName, file.getInputStream(), objectMetadata)
+                            .withCannedAcl(CannedAccessControlList.PublicRead)
+            );
+
+            return amazonS3Client.getUrl(bucket, originName).toString();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            amazonS3Client.shutdown();
+        }
+        return null;
+    }
+
+    public String getImageUrl(Long postId) {
+        try {
+            return amazonS3Client.getUrl(bucket, "image/image_"+postId).toString();
+        } catch (AmazonS3Exception e) {
+            return null;
+        } finally {
+            amazonS3Client.shutdown();
+        }
+    }
+}
