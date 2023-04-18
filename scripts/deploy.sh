1896198d68b23a6e810c27d4a93552cb42323e12
jiseok419 <pyosik0419@gmail.com>
2023-04-18 16:00:20 +0900

    :construction_worker: :: deploy.sh 프로세스 pid 삭제 수정

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,20 +1,22 @@
 #!/usr/bin/env bash
 
 REPOSITORY=/home/ubuntu/app
+PROJECT_NAME=proteen
 
 echo "> 현재 구동 중인 애플리케이션 pid 확인"
 
-CURRENT_PID=$(pgrep -fl action | grep java | awk '{print $1}')
+CURRENT_PID=$(pgrep -fl $PROJECT_NAME | grep java | awk '{print $1}')
 
 echo "현재 구동 중인 애플리케이션 pid: $CURRENT_PID"
 
-if [ -z "$CURRENT_PID" ]; then
-  echo "현재 구동 중인 애플리케이션이 없으므로 종료하지 않습니다."
+if [ -z $CURRENT_PID ]; then
+    echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다."
 else
-  echo "> kill -15 $CURRENT_PID"
-  kill -15 $CURRENT_PID
-  sleep 5
+    echo "> kill -15 $CURRENT_PID"
+    kill -15 $CURRENT_PID
+    sleep 5
 fi
+
 
 echo "> 새 애플리케이션 배포"
 
@@ -24,7 +26,7 @@ echo "> JAR NAME: $JAR_NAME"
 
 echo "> $JAR_NAME 에 실행권한 추가"
 
-chmod +x $JAR_NAME
+sudo chmod +x $JAR_NAME
 
 echo "> $JAR_NAME 실행"
 
